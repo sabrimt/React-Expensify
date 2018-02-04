6da45faf44561341e6c3c588b0ebfe62390b9196
sabri <sabrimtir9@gmail.com>
2018-02-04 15:05:42 +0100

    Setup production webpack build

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -23,7 +23,7 @@ export class AddExpensePage extends React.Component
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addExpense: (expense) => dispath(addExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
